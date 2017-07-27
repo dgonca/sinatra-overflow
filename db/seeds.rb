6bbb98feb46b002952e71b97c5a3b19542741813
Dominique Goncalves & Mark Franciose & Ravi R Patel & Tony Rodriguez <pair+dgonca.markfranciose.rrpatel25.tony-rodriguez@devbootcamp.com>
2017-07-27 17:09:09 -0500

    Seed the db take 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,9 @@ end
 25.times do
   question = Question.create!(
     title: Faker::Company.catch_phrase + " " + Faker::Company.catch_phrase,
-    content: ";lkasjnfg kl;adsjng k;lgnds;f gjkln adsflkngm dsfl;kjbn dsl;kgn dflgmv d;'kmds l;knbdfs lkm   glrjdsflig jdsflkvn dfgbjkn dfl;kgv jbn dlfbkn er'lg dfblvkndf blovdfhj lobdknf q'efkl pivhueor; gkjndf
-    ;bvlk edgij ogbivn ;obkf gjop'ertojgh iobnj rtpobjrgop[km ;
-    lgbfdm lkbnds;fgk
-     [gkdfsmlb ndsflkbmn df;lbmvd;fods g;dofgbm gjo;d sbdfo;j hdops[jb dgop[bj dsbo;j topr[wj d;fbovjk eqrgopj ldir gj;dfsbk dpifbvn dslfbmng eopgisdfa[pikveprgmdfslkmvafs;'lgjepqrobjdarlkgnmer;hoksdf
-      [vmdlrkgnosjgkjasgrl;kJnbgkjasrjfgl'asehglkqsdjnfv;'ljsak;jdth4r;
-        gjal'asknmg v    asgrjg;kljerov;jm",
+    content: "Spicy jalapeno bacon ipsum dolor amet brisket picanha pig biltong hamburger frankfurter capicola kevin pancetta strip steak. Venison turkey andouille, ribeye tongue tenderloin pastrami swine shankle meatball pig leberkas. Pastrami tongue pig bacon jerky porchetta, doner swine short loin burgdoggen ball tip filet mignon leberkas jowl turducken. Short ribs kielbasa pastrami pork turducken doner. Short loin meatball picanha landjaeger shoulder bacon porchetta alcatra frankfurter shankle pork chop. Jowl ball tip meatball, strip steak doner pancetta rump tenderloin pork belly pork brisket biltong venison. Ham jowl spare ribs, shankle chuck meatball pork chop ball tip ground round t-bone.
+
+      Pork chop ball tip bacon, ham hock pig kielbasa fatback short loin burgdoggen hamburger sausage. T-bone drumstick landjaeger, pig fatback flank cow shankle filet mignon. Jerky strip steak porchetta, biltong t-bone sausage pork jowl kevin pork chop shoulder turducken frankfurter bresaola. Pancetta shoulder kielbasa tri-tip tail picanha corned beef. Filet mignon biltong chuck tenderloin pork belly. Strip steak sirloin short loin meatball andouille tri-tip spare ribs.",
     author_id: rand(22) + 1
     )
     question.comments.create!(content: Faker::TwinPeaks.quote, author_id: rand(22) + 1)
@@ -25,10 +22,9 @@ end
 
   25.times do
     answer = Answer.create!(
-      content: "lgbfdm lkbnds;fgk
-     [gkdfsmlb ndsflkbmn df;lbmvd;fods g;dofgbm gjo;d sbdfo;j hdops[jb dgop[bj dsbo;j topr[wj d;fbovjk eqrgopj ldir gj;dfsbk dpifbvn dslfbmng eopgisdfa[pikveprgmdfslkmvafs;'lgjepqrobjdarlkgnmer;hoksdf
-      [vmdlrkgnosjgkjasgrl;kJnbgkjasrjfgl'asehglkqsdjnfv;'ljsak;jdth4r;
-        gjal'asknmg v    asgrjg;kljerov;jm",
+      content: "Pig turducken strip steak alcatra chicken brisket t-bone kielbasa porchetta ball tip, andouille rump. Short loin cupim ball tip bresaola beef pork belly burgdoggen cow. Cupim tail beef kevin jerky prosciutto, fatback t-bone spare ribs hamburger bresaola salami pork chop. Pork boudin jerky prosciutto tri-tip turkey meatloaf pastrami. Alcatra ground round flank, ham hock spare ribs drumstick pork chop meatball turkey kevin capicola turducken tongue. Brisket drumstick meatball, spare ribs sausage tongue beef ribs tail corned beef. Pork loin porchetta tail short ribs corned beef ham hock jowl tri-tip pork belly filet mignon burgdoggen. Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!
+
+        ",
         author_id: rand(22) + 1,
         question_id: rand(22) + 1
       )
