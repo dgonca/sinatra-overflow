8cb907e2b80a1cfe8879390064328320c46f1574
Dominique Goncalves & Mark Franciose & Ravi R Patel & Tony Rodriguez <pair+dgonca.markfranciose.rrpatel25.tony-rodriguez@devbootcamp.com>
2017-07-28 15:15:10 -0500

    add question show page and fix some styling

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $(".logout").on("click", function(event){
+    event.preventDefault();
+
+    $.ajax({
+      method: "DELETE",
+      url: "/sessions"
+    })
+    .done(function(){
+      location.reload(true);
+    })
+  })
+
 });
