7229f857e5b62a33d0eaa2e1a4cd537cdfcc480f
Mark Franciose & Ravi R Patel <pair+markfranciose.rrpatel25@devbootcamp.com>
2017-07-28 14:28:30 -0500

    after lunch status

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,4 +1,5 @@
 get '/questions' do
+  "What?"
   # show all questions
   # on the board we wrote this route as "/questions/index"
   # erb:'questions/show'
@@ -13,8 +14,21 @@ post '/questions' do
   # should it redirect to '/questions/:id' or to '/questions' ?
 end
 
-get 'questions/:id' do
-  # display page for individual question
+get '/questions/:id' do
+  "Hi"# display page for individual question
+end
+
+post '/questions/:id/answers' do
+  if request.xhr? && logged_in?
+    @answer = Answer.new(content: params[:content], question_id: params[:id], author_id: current_user.id)
+    if @answer.save
+      erb :"/questions/show", layout: false
+    else
+      errors = @answer.errors.full_messages
+    end
+  else
+    "something is fucked up"
+  end
 end
 
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  var button = $("#sendbutton")
+  var content = $("#submitanswer")
+  var link  = $("#dropbox")
+  button.on("click", function(event) {
+    event.preventDefault();
+    $.ajax( {
+      method: "POST",
+      url: "/questions/1/answers", //get correct id
+      data: { content: $("#theanswer").val()} //capture correct input
+    }).done(function(response) {
+      content.hide();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+      $(response).appendTo($("#dropbox"))
+    })
+  });
+
 });
